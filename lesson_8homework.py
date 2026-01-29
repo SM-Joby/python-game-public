6e60d918f8bb5becef9a1c6de54a2c71332cb57f
SM-Joby <mail2smjoby@gmail.com>
2026-01-29 18:54:05 +0000

    Add files via upload

---
+++ lesson_8homework.py
@@ -0,0 +1,28 @@
+'''savannah = 80,90,70,60 
+a,b,c,d = savannah
+print("Savannah")
+print(a)
+print(b)
+print(c)
+print(d)'''
+
+s1 = "John", 76,83,46
+name,math,english,irish = s1
+
+average = math+english+irish/3
+
+print("student report card")
+print("name : ",name)
+print("maths : ", math)
+print("english : ", english)
+print("irish : ",irish)
+print("average : ", average)
+print("teachers comment: john is doing very well in english compared to last year but his irish is dropping")
+
+
+c1 = "germany", "berlin wall", "fairytale country", "capital is berlin"
+c2 = "switzerland", "churches", "home of chocolate", "capital is vienna"
+c3 = "UK", "london eye","british crown", "capital is london"
+c4 = "ireland","home of guiness","cliffs of moher","capital is dublin"
+
+
