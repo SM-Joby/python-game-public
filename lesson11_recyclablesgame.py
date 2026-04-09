3fefcee65d58915fc11b6b484bd4e38a170c0765
SM-Joby <mail2smjoby@gmail.com>
2026-04-09 18:55:06 +0100

    Add files via upload

--- lesson11_recyclablesgame.py
+++ lesson11_recyclablesgame.py
@@ -14,11 +14,37 @@ bin.pos = (400,670)
 
 def draw():
     screen.clear()
-    screen.fill(color = (0,0,225))
+    screen.fill(color = (0,0,225))# brackets are given since the rgb numbers are tuple
     item.draw()
     bin.draw()
     screen.draw.text("Score: "+ str(score), (0,0),  fontsize = 20 )
 
+def update():
+    global item,score
+    item.y = item.y+4
+
+    if keyboard.left:
+        bin.x = bin.x-2
+    elif keyboard.right:
+        bin.x = bin.x+2
+    
+    if bin.colliderect(item):
+        if item.image == "can":
+            score = score+10
+        elif item.image == "paper":
+            score = score+10
+        elif item.image == "bulb":
+            score = score-10
+        elif item.image == "battery":
+            score = score-10
+    
+        item = Actor(random.choice(waste))
+        item.pos = (random.randint(0,775),0)
+
+    if item.y>HEIGHT:
+        item = Actor(random.choice(waste))
+        item.pos = (random.randint(0,775),0)
+        
 
 
 pgzrun.go() #dont forget it is a function
